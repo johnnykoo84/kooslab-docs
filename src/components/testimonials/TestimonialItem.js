f8f7cd9b8cbc76427ff87bd5afd7692e0986dc08
johnnykoo84 <johnnykoo84@gmail.com>
2024-10-29 11:46:35 +0900

    Adjust text to center and update pricing contents

--- src/components/testimonials/TestimonialItem.js
+++ src/components/testimonials/TestimonialItem.js
@@ -4,7 +4,7 @@ const Testimonial = ({ logo, testimonial_1, testimonial_2, client }) => {
   return (
     <div className="flex flex-col items-center border mb-4 p-2">
       <img src={logo} className="bg-white p-4 mb-2" alt={`${client} logo`} />
-      <div className="text-left mb-2">
+      <div className="text-left sm:px-36 mb-2">
         <p>{testimonial_1}</p>
         <p>{testimonial_2}</p>
       </div>
